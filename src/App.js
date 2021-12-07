50129875551dcd2ac89cd0382c4febe0b2605acb
Fosa1990 <fosa1990@gmail.com>
2021-12-07 17:16:37 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h2>Still nothing here Σ(‘◉⌓◉’) </h2>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
